b0550a30435169306f69013ba723ede0671ee6c3
Armand Araujo <armandaraujo@Armands-MacBook-Pro.local>
2023-11-07 16:08:52 -0800

    added dashboard.js edited dashboard.handlebars file

---
+++ public/js/dashboard.js
@@ -0,0 +1,78 @@
+function handlePostSubmit (e) {
+    e.preventDefault()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const title = document.querySelector('#post-title').value.trim();
+ 
+
+  if (title && needed_funding && description) {
+    const response = await fetch(`/api/projects`, {
+      method: 'POST',
+      body: JSON.stringify({ title, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create project');
+    }
+  };
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/projects/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete project');
+    }
+  }
+};
+
+document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler);
+
+
+document.querySelector(".submission")
+.addEventListener("submit", handlePostSubmit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
